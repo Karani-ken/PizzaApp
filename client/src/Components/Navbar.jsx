d2b58a660cbeb1e94c0bf52413ba62513dfede1d
Karani-ken <academice79@gmail.com>
2022-12-24 12:17:54 +0300

    init

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg">
-                <a className="navbar-brand" href="#">Navbar</a>
+            <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
+                <a className="navbar-brand" href="#">Shey Pizza</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse " id="navbarNav">
                     <ul className="navbar-nav ml-auto">
-                        <li N="nav-item active">
-                            <a className="nav-link" href="#">Home</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Login </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="#">Cart</a>
                         </li>
                         
                     </ul>
---
+++ client/src/pizzasData.js
@@ -0,0 +1,92 @@
+[
+    {
+        name:"PEPPER BARBECUE CHICKEN",
+        variants:["small","medium","large"],
+        prices:[
+            {
+                small: 200,
+                medium: 350,
+                large:400,
+            },
+        ],
+        category:"nonveg",
+        image:"https://chefchoice.com.np/wp-content/uploads/2020/07/Pepper_Barbeque.jpg",
+        description:"Pepper Barbecue Chicken I Cheese",
+
+    },
+    {
+        name:"Non Veg Supreme",
+        variants:["small","medium","large"],
+        prices:[
+            {
+                small: 200,
+                medium: 350,
+                large:400,
+            },
+        ],
+        category:"nonveg",
+        image:"https://i.pinimg.com/736x/0b/80/52/0b805258584043ba55080732a30e6d16.jpg",
+        description:"Pepper Barbecue Chicken I Cheese",
+
+    },
+    {
+        name:"Golden Corn Pizza",
+        variants:["small","medium","large"],
+        prices:[
+            {
+                small: 200,
+                medium: 350,
+                large:400,
+            },
+        ],
+        category:"veg",
+        image:"https://i.pinimg.com/736x/0b/80/52/0b805258584043ba55080732a30e6d16.jpg",
+        description:"Pepper Barbecue Chicken I Cheese",
+
+    },
+    {
+        name:"Chicken Pizza",
+        variants:["small","medium","large"],
+        prices:[
+            {
+                small: 200,
+                medium: 350,
+                large:400,
+            },
+        ],
+        category:"nonveg",
+        image:"https://i.pinimg.com/736x/0b/80/52/0b805258584043ba55080732a30e6d16.jpg",
+        description:"Pepper Barbecue Chicken I Cheese",
+
+    },
+    {
+        name:"Green Starch Pizza",
+        variants:["small","medium","large"],
+        prices:[
+            {
+                small: 200,
+                medium: 350,
+                large:400,
+            },
+        ],
+        category:"veg",
+        image:"https://i.pinimg.com/736x/0b/80/52/0b805258584043ba55080732a30e6d16.jpg",
+        description:"Pepper Barbecue Chicken I Cheese",
+
+    },
+    {
+        name:"Chilly Spiced Pork Pizza",
+        variants:["small","medium","large"],
+        prices:[
+            {
+                small: 200,
+                medium: 350,
+                large:400,
+            },
+        ],
+        category:"nonveg",
+        image:"https://i.pinimg.com/736x/0b/80/52/0b805258584043ba55080732a30e6d16.jpg",
+        description:"Pepper Barbecue Chicken I Cheese",
+
+    },
+]
